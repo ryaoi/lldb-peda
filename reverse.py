7252169c234adf185fce6a363672711273e1d512
ryota <ryota@42.codes>
2021-01-16 18:13:52 +0900

    refactor code

--- reverse.py
+++ reverse.py
@@ -1,103 +1,118 @@
-def reverse(debugger, command, result, internal_dict):
-    target = debugger.GetSelectedTarget()
-    print("FT_" + str(target)[::-1])
 
-def tohex(val, nbits):
-      return hex((val + (1 << nbits)) % (1 << nbits))
+from functools import wraps
 
-def find_value(process, first_value):
-    arrow = []
-    if "0x00007f" in first_value:
-        arrow.append(first_value)
-        value = process.GetSelectedThread().GetFrameAtIndex(0).EvaluateExpression('*(long*)' + first_value).unsigned
-        arrow.append(str(hex(value)))
-        i = 1
-        while "0x7fff" in arrow[i]:
-            value = process.GetSelectedThread().GetFrameAtIndex(0).EvaluateExpression('*(long*)' + arrow[i]).unsigned
-            arrow.append(str(hex(value)))
-            i += 1
-        if len(arrow) > 1:
-            arrow[len(arrow) - 1] = str(hex(process.GetSelectedThread().GetFrameAtIndex(0).EvaluateExpression('*(int*)' + arrow[len(arrow) - 1]).unsigned))
+OUTPUT_REGISTERS = [
+    'rax',
+    'rbx',
+    'rcx',
+    'rdx',
+    'rsi',
+    'rsi',
+    'rdi',
+    'rbp',
+    'rsp',
+    'rip'
+]
 
-    elif "0x7fff" in first_value:
-        arrow.append(first_value)
-        value = process.GetSelectedThread().GetFrameAtIndex(0).EvaluateExpression('*(long*)' + first_value).unsigned
-        arrow.append(str(hex(value)))
-        i = 1
-        while "0x7fff" in arrow[i]:
-            value = process.GetSelectedThread().GetFrameAtIndex(0).EvaluateExpression('*(long*)' + arrow[i]).unsigned
-            arrow.append(str(hex(value)))
-            i += 1
-        if len(arrow) > 1:
-            arrow[len(arrow) - 1] = str(hex(process.GetSelectedThread().GetFrameAtIndex(0).EvaluateExpression('*(long*)' + arrow[len(arrow) - 1]).unsigned))
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
-    return arrow
+# https://gist.github.com/stek29/cdbbbe018f0aaf0b2a9a58c9173becb8
+RFLAGS = [
+	['CF', 'Carry Flag', 'carry'],
+	[None, 'Reserved', None],
+	['PF', 'Parity Flag', 'parity'],
+	[None, 'Reserved', None],
+	['AF', 'Adjust Flag', 'adjust'],
+	[None, 'Reserved', None],
+	['ZF', 'Zero Flag', 'zero'],
+	['SF', 'Sign Flag', 'sign'],
+	['TF', 'Trap Flag', 'trap'],
+	['IF', 'Interrupt Enable Flag', 'interrupt'],
+	['DF', 'Direction Flag', 'direction'],
+	['OF', 'Overflow Flag', 'overflow'],
+	['IOPL_H', 'I/O privilege level High bit', 'IOPL_H'],
+	['IOPL_L', 'I/O privilege level Low bit', 'IOPL_L'],
+	['NT', 'Nested Task Flag', 'nested'],
+	[None, 'Reserved', None],
 
-def reg(debugger, command, result, internal_dict):
-    print('\033[94m[------------------registers---------------]\033[0m')
+	# eflags
+	['RF', 'Resume Flag', 'resume'],
+	['VM', 'Virtual 8086 mode flag', 'VM'],
+	['AC', 'Alignment check', 'alignement'],
+	['VIF', 'Virtual interrupt flag', 'virtual interrpt'],
+	['VIP', 'Virtual interrupt pending', 'VIP'],
+	['ID', 'Able to use CPUID instruction', 'ID'],
+	# 22-31 reserved
+
+	# rflags 32-63 reserved
+]
+
+def ParseRflags(val):
+    """ Returns list of set flags """
+    rflags = list()
+
+    for bit, desc in enumerate(RFLAGS):
+        shortname, name, output = desc
+        if val & (1 << bit) and name != 'Reserved':
+            rflags.append(output)
+
+    return rflags
+
+def FormatOutput(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f"{bcolors.OKBLUE}[{func.__name__.center(60, '-')}]{bcolors.ENDC}")
+        results = func(*args, **kwargs)
+        print(f"{bcolors.OKBLUE}[{'-'*60}]{bcolors.ENDC}")
+        return results
+    return wrapper
+
+@FormatOutput
+def Registers(debugger, command, result, internal_dict):
     target = debugger.GetSelectedTarget()
     process = target.GetProcess()
     mainThread = process.GetThreadAtIndex(0)
     currentFrame = mainThread.GetSelectedFrame()
     registerList = currentFrame.GetRegisters()
-    for value in registerList:
-        for child in value:
-            if child.GetName() in ['rax', 'rbx', 'rcx', 'rdx', 'rsi', 'rsi', 'rdi', 'rbp', 'rsp', 'rip', 'rflags']:
-                if child.GetName() == 'rflags':
-                    flags = []
-                    if int(child.GetValue(), 0) & 0x0001:
-                        flags.append("carry")
-                    if int(child.GetValue(), 0) & 0x0004:
-                        flags.append("parity")
-                    if int(child.GetValue(), 0) & 0x0010:
-                        flags.append("adjust")
-                    if int(child.GetValue(), 0) & 0x0040:
-                        flags.append("zero")
-                    if int(child.GetValue(), 0) & 0x0080:
-                        flags.append("sign")
-                    if int(child.GetValue(), 0) & 0x0100:
-                        flags.append("trap")
-                    if int(child.GetValue(), 0) & 0x0200:
-                        flags.append("interrupt")
-                    if int(child.GetValue(), 0) & 0x0400:
-                        flags.append("direction")
-                    if int(child.GetValue(), 0) & 0x0800:
-                        flags.append("overflow")
+    generalPurposeRegister = registerList[0]
+    for registers in registerList:
+        for register in registers:
+            regName = register.GetName()
+            if regName in OUTPUT_REGISTERS:
+                    print(f"{bcolors.OKGREEN}{regName}{bcolors.ENDC}: {register.GetValue()}")
+            elif regName == 'rflags':
+                regValue = int(register.GetValue(), 0)
+                rflags = ParseRflags(regValue)
+                colorizeFlagsOutput = [f"{bcolors.OKGREEN}{flag}" if index % 2 else f"{bcolors.FAIL}{flag.upper()}" for index, flag in enumerate(rflags)]
+                print(f"{bcolors.OKGREEN}rflags {bcolors.ENDC}: {register.GetValue()}({' | '.join(colorizeFlagsOutput)}{bcolors.ENDC})")
 
-                    flags_peda = ['\033[92m' + x if i % 2 else '\033[91m' + x.upper() for i, x in enumerate(flags)]
-                    print('\033[92mrflags \033[0m: ' +child.GetValue().replace("000000000000", "") + ' (' + " | ".join(flags_peda) + '\033[0m)')
-                else:
-                    arrow = find_value(process, child.GetValue())
-                    if any(arrow):
-                        print('\033[92m' + child.GetName() + "\033[0m: " + " --> ".join(arrow))
-                    else:
-                        print('\033[92m' + child.GetName() + "\033[0m: " + child.GetValue())
-
-    print('\033[94m[------------------------------------------]\033[0m')
-
-def code(debugger, command, result, internal_dict):
-    print('\033[94m[--------------------Code------------------]\033[0m')
-    cur_pc = debugger.GetSelectedTarget().GetProcess().GetSelectedThread().GetSelectedFrame().GetPC();
+@FormatOutput
+def Code(debugger, command, result, internal_dict):
+    cur_pc = debugger.GetSelectedTarget().GetProcess().GetSelectedThread().GetSelectedFrame().GetPC()
     debugger.HandleCommand('disassemble --start-address=' + str(cur_pc) + ' -c 4')
-    print('\033[94m[------------------------------------------]\033[0m')
 
-def stack(debugger, command, result, internal_dict):
-    print('\033[94m[-------------------Stack------------------]\033[0m')
-    target = debugger.GetSelectedTarget()
-    process = target.GetProcess()
-    cur_sp = process.GetSelectedThread().GetSelectedFrame().GetSP();
+@FormatOutput
+def Stack(debugger, command, result, internal_dict):
+    cur_sp = debugger.GetSelectedTarget().GetProcess().GetSelectedThread().GetSelectedFrame().GetSP()
     debugger.HandleCommand('x/12gx ' + str(cur_sp))
-    print('\033[94m[------------------------------------------]\033[0m')
 
-def peda(debugger, command, result, internal_dict):
-    reg(debugger, command, result, internal_dict)
-    code(debugger, command, result, internal_dict)
-    stack(debugger, command, result, internal_dict)
+def Peda(debugger, command, result, internal_dict):
+    Registers(debugger, command, result, internal_dict)
+    Code(debugger, command, result, internal_dict)
+    Stack(debugger, command, result, internal_dict)
 
 def __lldb_init_module(debugger, internal_dict):
-    debugger.HandleCommand('command script add -f reverse.reverse reverse')
-    debugger.HandleCommand('command script add -f reverse.reg reg')
-    debugger.HandleCommand('command script add -f reverse.code code')
-    debugger.HandleCommand('command script add -f reverse.stack stack')
-    debugger.HandleCommand('command script add -f reverse.peda peda')
+    debugger.HandleCommand('command script add -f peda.Registers reg')
+    debugger.HandleCommand('command script add -f peda.Code code')
+    debugger.HandleCommand('command script add -f peda.Stack stack')
+    debugger.HandleCommand('command script add -f peda.Peda peda')
 
